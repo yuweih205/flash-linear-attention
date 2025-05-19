4686c41cea01946d5d49db37f6e3aa36e91dc75d
Yu Zhang <yzhang.cs@outlook.com>
2025-05-20 00:24:49 +0800

    [GSA] Allow for GQA inference with repeat (#408)

--- fla/layers/gsa.py
+++ fla/layers/gsa.py
@@ -9,7 +9,7 @@ from typing import TYPE_CHECKING, Dict, Optional, Tuple
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from einops import rearrange
+from einops import rearrange, repeat
 
 from fla.layers.utils import get_unpad_data, index_first_axis, pad_input
 from fla.modules import RMSNorm, ShortConvolution
@@ -177,6 +177,9 @@ class GatedSlotAttention(nn.Module):
 
         f = F.logsigmoid(f) / self.gate_logit_normalizer
         s = (1 - f.exp()).to(f.dtype)
+
+        if self.num_kv_groups > 1:
+            k, v, f, s = map(lambda x: repeat(x, '... h d -> ... (h g) d', g=self.num_kv_groups), (k, v, f, s))
 
         recurrent_state = last_state['recurrent_state'] if last_state is not None else None
         if mode == 'fused_recurrent':
