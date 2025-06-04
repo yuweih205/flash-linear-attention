[RWKV] Fix `cu_seqlens` with gradient checkpoint (#422)

fla/layers/rwkv6.py
@@ -105,6 +105,7 @@ class RWKV6Attention(nn.Module):
         past_key_values: Optional[Cache] = None,
         use_cache: Optional[bool] = False,
         output_attentions: Optional[bool] = False,
+        cu_seqlens: Optional[torch.LongTensor] = None,
         **kwargs
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Cache]]:
         if attention_mask is not None:
@@ -124,7 +125,7 @@ class RWKV6Attention(nn.Module):
 
         if attention_mask is not None:
             hidden_states = hidden_states.mul_(attention_mask[:, -hidden_states.shape[-2]:, None])
-        cu_seqlens = kwargs.get('cu_seqlens', None)
+
         if hidden_states.shape[1] == 1 and last_state is not None:
             shifted = last_state['conv_state'].unsqueeze(1)
             delta = shifted - hidden_states
@@ -312,7 +313,7 @@ class LerpLinear(nn.Module):
         return s
 
     def forward(self, x: torch.Tensor, delta: Optional[torch.Tensor] = None,
-                cu_seqlens: Optional[torch.Tensor] = None) -> torch.Tensor:
+                cu_seqlens: Optional[torch.LongTensor] = None) -> torch.Tensor:
         if delta is None:
             delta = token_shift(x, cu_seqlens)
         return self.linear(x + delta * self.mu)
@@ -347,7 +348,7 @@ class DDLerpLinear(nn.Module):
 
     def forward(self, x: torch.Tensor, mu: torch.Tensor,
                 delta: Optional[torch.Tensor] = None,
-                cu_seqlens: Optional[torch.Tensor] = None) -> torch.Tensor:
+                cu_seqlens: Optional[torch.LongTensor] = None) -> torch.Tensor:
         if delta is None:
             delta = token_shift(x, cu_seqlens)
         return self.linear(x + delta * mu)

fla/layers/rwkv7.py
@@ -200,6 +200,7 @@ class RWKV7Attention(nn.Module):
         use_cache: Optional[bool] = False,
         output_attentions: Optional[bool] = False,
         v_first: torch.Tensor = None,
+        cu_seqlens: Optional[torch.LongTensor] = None,
         **kwargs
     ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Cache]]:
         if attention_mask is not None:
@@ -217,7 +218,7 @@ class RWKV7Attention(nn.Module):
 
         if attention_mask is not None:
             hidden_states = hidden_states.mul(attention_mask[:, -seq_len:, None])
-        cu_seqlens = kwargs.get('cu_seqlens', None)
+
         # delta [batch_size, seq_len, hidden_size]
         if last_state is None:
             delta = token_shift(hidden_states, cu_seqlens)

fla/models/rwkv6/modeling_rwkv6.py
@@ -64,6 +64,7 @@ class RWKV6FeedForward(nn.Module):
         x: torch.Tensor,
         attention_mask: Optional[torch.Tensor] = None,
         state: Optional[Cache] = None,
+        cu_seqlens: Optional[torch.LongTensor] = None,
         **kwargs
     ) -> torch.Tensor:
         if attention_mask is not None:
@@ -77,7 +78,6 @@ class RWKV6FeedForward(nn.Module):
                 shifted[:, 0] = state[self.layer_idx]['ffn_state']
             delta = shifted - x
         else:
-            cu_seqlens = kwargs.get('cu_seqlens', None)
             delta = token_shift(x, cu_seqlens)
         key = self.act_fn(self.key(x, delta))
         value = self.value(key)
@@ -151,6 +151,7 @@ class RWKV6Block(nn.Module):
         past_key_values: Optional[Cache] = None,
         use_cache: Optional[bool] = False,
         output_attentions: Optional[bool] = False,
+        cu_seqlens: Optional[torch.LongTensor] = None,
         **kwargs,
     ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
         residual = self.pre_norm(hidden_states) if hasattr(self, 'pre_norm') else hidden_states
@@ -161,6 +162,7 @@ class RWKV6Block(nn.Module):
             past_key_values=past_key_values,
             use_cache=use_cache,
             output_attentions=output_attentions,
+            cu_seqlens=cu_seqlens,
             **kwargs
         )
         if self.config.fuse_norm:
@@ -169,7 +171,9 @@ class RWKV6Block(nn.Module):
             hidden_states = residual + hidden_states
             residual = hidden_states
             hidden_states = self.ffn_norm(hidden_states)
-        hidden_states, past_key_values = self.ffn(hidden_states, attention_mask, past_key_values, **kwargs)
+        hidden_states, past_key_values = self.ffn(
+            hidden_states, attention_mask, past_key_values, cu_seqlens, **kwargs
+        )
         hidden_states = residual + hidden_states
 
         outputs = (hidden_states, attentions, past_key_values)
@@ -264,6 +268,7 @@ class RWKV6Model(RWKV6PreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
+        cu_seqlens: Optional[torch.LongTensor] = None,
         **kwargs: Unpack[Dict]
     ) -> Union[Tuple, BaseModelOutputWithPast]:
         if output_attentions:
@@ -305,6 +310,7 @@ class RWKV6Model(RWKV6PreTrainedModel):
                     past_key_values,
                     use_cache,
                     output_attentions,
+                    cu_seqlens,
                     **kwargs
                 )
             else:
@@ -314,6 +320,7 @@ class RWKV6Model(RWKV6PreTrainedModel):
                     past_key_values=past_key_values,
                     use_cache=use_cache,
                     output_attentions=output_attentions,
+                    cu_seqlens=cu_seqlens,
                     **kwargs
                 )
 

fla/models/rwkv7/modeling_rwkv7.py
@@ -82,6 +82,7 @@ class RWKV7FeedForward(nn.Module):
         x: torch.Tensor,
         attention_mask: Optional[torch.Tensor] = None,
         state: Optional[Cache] = None,
+        cu_seqlens: Optional[torch.LongTensor] = None,
         **kwargs
     ) -> torch.Tensor:
         if attention_mask is not None:
@@ -94,7 +95,6 @@ class RWKV7FeedForward(nn.Module):
             shifted[:, 0] = state[self.layer_idx]['ffn_state'][-1]
             delta = shifted - x
         else:
-            cu_seqlens = kwargs.get('cu_seqlens', None)
             delta = token_shift(x, cu_seqlens)
         if state is not None:
             # no need to update the offset twice
@@ -174,6 +174,7 @@ class RWKV7Block(nn.Module):
         use_cache: Optional[bool] = False,
         output_attentions: Optional[bool] = False,
         v_first: torch.Tensor = None,
+        cu_seqlens: Optional[torch.LongTensor] = None,
         **kwargs,
     ) -> Tuple[torch.FloatTensor, Optional[Tuple[torch.FloatTensor, torch.FloatTensor]]]:
         residual = self.pre_norm(hidden_states) if hasattr(self, 'pre_norm') else hidden_states
@@ -185,6 +186,7 @@ class RWKV7Block(nn.Module):
             use_cache=use_cache,
             output_attentions=output_attentions,
             v_first=v_first,
+            cu_seqlens=cu_seqlens,
             **kwargs
         )
         if self.config.fuse_norm:
@@ -193,7 +195,9 @@ class RWKV7Block(nn.Module):
             hidden_states = residual + hidden_states
             residual = hidden_states
             hidden_states = self.ffn_norm(hidden_states)
-        hidden_states, past_key_values = self.ffn(hidden_states, attention_mask, past_key_values, **kwargs)
+        hidden_states, past_key_values = self.ffn(
+            hidden_states, attention_mask, past_key_values, cu_seqlens, **kwargs
+        )
         hidden_states = residual + hidden_states
 
         outputs = (hidden_states, attentions, past_key_values, v_first)
@@ -351,6 +355,7 @@ class RWKV7Model(RWKV7PreTrainedModel):
         output_attentions: Optional[bool] = None,
         output_hidden_states: Optional[bool] = None,
         return_dict: Optional[bool] = None,
+        cu_seqlens: Optional[torch.LongTensor] = None,
         **kwargs: Unpack[Dict]
     ) -> Union[Tuple, BaseModelOutputWithPast]:
         if output_attentions:
@@ -395,6 +400,7 @@ class RWKV7Model(RWKV7PreTrainedModel):
                     use_cache,
                     output_attentions,
                     v_first,
+                    cu_seqlens,
                     **kwargs
                 )
             else:
@@ -405,6 +411,7 @@ class RWKV7Model(RWKV7PreTrainedModel):
                     use_cache=use_cache,
                     output_attentions=output_attentions,
                     v_first=v_first,
+                    cu_seqlens=cu_seqlens,
                     **kwargs
                 )
 